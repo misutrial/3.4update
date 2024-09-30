8185db2f8fbc76810c64a29ddac6c8a42deb9934
misutrial <misutrial@gmail.com>
2024-10-01 00:08:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7424017593:AAEZBI7cxIBekjnLxURqCogXz3jjGXXp30I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@technicalguruji113'')
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5399575222")
+
+
+
+
+
+  
